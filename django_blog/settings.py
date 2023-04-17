f7efe1dd5f7e622cc3cabbf712af56d692967ac3
Paiman Rasoli <paimanrasoli789@gmail.com>
2023-04-17 21:39:52 +0430

    add graphene and create post model

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "post.apps.PostConfig",
+    'graphene_django',
+
 ]
 
 MIDDLEWARE = [
--- post/models.py
+++ post/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+      id = models.AutoField(primary_key=True,)
+      title = models.CharField(max_length=255)
+      description = models.TextField(max_length=1000)
