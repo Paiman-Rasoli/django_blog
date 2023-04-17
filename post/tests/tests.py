8b4b18e4c8bd1a83a33dcafe065c3e1cbe9a0b23
Paiman Rasoli <paimanrasoli789@gmail.com>
2023-04-17 23:37:43 +0430

    create the first test

--- post/tests/tests.py
+++ post/tests/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from post.models import Post
 
 class TestPostModel(TestCase):
       def test_post_has_title(self):
-            pass
+            post = Post.objects.create(title="Paiman")
+            self.assertTrue(len(post.title) > 0)            
